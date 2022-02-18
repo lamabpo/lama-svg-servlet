1dd767af1ead57dd9291e43ea578f95106ae3be3
Giedrius Deveikis <giedrius.deveikis@lamabpo.lt>
2022-02-18 18:14:12 +0200

    Fix open files leak

--- src/main/java/lt/lamabpo/svg/SVG2PNGServlet.java
+++ src/main/java/lt/lamabpo/svg/SVG2PNGServlet.java
@@ -24,20 +24,23 @@ import org.apache.batik.transcoder.TranscoderInput;
 import org.apache.batik.transcoder.TranscoderOutput;
 
 import java.io.File;
+import java.io.InputStream;
 import java.io.IOException;
 import java.io.StringReader;
 import java.awt.GraphicsEnvironment;
 import java.awt.Font;
 import java.awt.FontFormatException;
+import static java.lang.Math.toIntExact;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.io.IOUtils;
+import javax.servlet.http.Part;
 import org.apache.commons.io.output.ByteArrayOutputStream;
 
 /**
@@ -55,64 +58,77 @@ public class SVG2PNGServlet extends HttpServlet {
         super.init(config);
         fontsDirectory = getServletContext().getInitParameter("fontsDirectory");
         try {
-            GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(fontsDirectory + "/Arimo-Regular.ttf")));
-        } catch (FontFormatException|IOException e) {
-            getServletContext().log("Unhandled exception while adding font from file",e);
+            GraphicsEnvironment.getLocalGraphicsEnvironment()
+                    .registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(fontsDirectory + "/Arimo-Regular.ttf")));
+        } catch (FontFormatException | IOException e) {
+            getServletContext().log("Unhandled exception while adding font from file", e);
         }
     }
+
     /**
      * Handles the HTTP <code>POST</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        try {
-            String test = null,dStr;
-            int w=0,h=0;
-            String svg;
-            if(request.getHeader("content-type").startsWith("multipart/form-data")){
-                svg = IOUtils.toString(request.getPart("svg").getInputStream(), "UTF-8");
-                try{
-                    test = IOUtils.toString(request.getPart("test").getInputStream(), "UTF-8");
-                }catch(IOException | NullPointerException e){
-                    try{
-                        dStr = IOUtils.toString(request.getPart("w").getInputStream(), "UTF-8");
-                        w = Integer.parseInt(dStr);
-                    }catch(NullPointerException n){}
-                    try{
-                        dStr = IOUtils.toString(request.getPart("h").getInputStream(), "UTF-8");
-                        h = Integer.parseInt(dStr);
-                    }catch(NullPointerException n){}
-                }
-            }else{
-                svg = request.getParameter("svg");
-                test = request.getParameter("test");
-                if(test==null||test.length()==0){
-                    w = Integer.parseInt(request.getParameter("w"));
-                    h = Integer.parseInt(request.getParameter("h"));
-                }
+        String test = null, dStr;
+        int w = 0, h = 0;
+        String svg;
+        if (request.getHeader("content-type").startsWith("multipart/form-data")) {
+            svg = partToString(request.getPart("svg"), null, "UTF-8");
+            test = partToString(request.getPart("test"), null, "UTF-8");
+            if (test == null || test.length() == 0) {
+                w = Integer.parseInt(partToString(request.getPart("w"), "0", "UTF-8"));
+                h = Integer.parseInt(partToString(request.getPart("h"), "0", "UTF-8"));
             }
+        } else {
+            svg = request.getParameter("svg");
+            test = request.getParameter("test");
+            if (test == null || test.length() == 0) {
+                w = Integer.parseInt(request.getParameter("w"));
+                h = Integer.parseInt(request.getParameter("h"));
+            }
+        }
+        try (ServletOutputStream sos = response.getOutputStream();
+                ByteArrayOutputStream os = new ByteArrayOutputStream()) {
             TranscoderInput ti = new TranscoderInput(new StringReader(svg));
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
             TranscoderOutput to = new TranscoderOutput(os);
             PNGTranscoder pngT = new PNGTranscoder();
-            if(w>0)
-                pngT.addTranscodingHint(ImageTranscoder.KEY_WIDTH,new Float(w));
-            if(h>0)
-                pngT.addTranscodingHint(ImageTranscoder.KEY_HEIGHT,new Float(h));
+            if (w > 0)
+                pngT.addTranscodingHint(ImageTranscoder.KEY_WIDTH, new Float(w));
+            if (h > 0)
+                pngT.addTranscodingHint(ImageTranscoder.KEY_HEIGHT, new Float(h));
             pngT.transcode(ti, to);
             os.flush();
             response.setContentType("image/png");
             response.setContentLength(os.size());
-            os.writeTo(response.getOutputStream());
-            response.getOutputStream().flush();
+            os.writeTo(sos);
+            sos.flush();
         } catch (TranscoderException ex) {
             throw new ServletException(ex);
+        }
+    }
+
+    /**
+     * 
+     * @param input       one part of multipart/form-data
+     * @param charsetName source charset of binary part data
+     * @return
+     * @throws IOException
+     */
+    public static String partToString(final Part input, final String defaultVal, final String charsetName)
+            throws IOException {
+        try (InputStream inputStream = input.getInputStream()) {
+            byte[] buffer = new byte[toIntExact(input.getSize())];
+            inputStream.read(buffer);
+            return new String(buffer,charsetName);
+        } catch (NullPointerException e) {
+            return defaultVal;
         }
     }
 }
